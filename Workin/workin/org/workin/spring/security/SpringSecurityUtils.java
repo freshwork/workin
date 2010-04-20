930b601d22b52241369296c4881214c16f78f367
goingmm@gmail.com <goingmm@gmail.com@243312a4-f120-11de-a719-13ef0a9ea6ad>
2010-04-20 12:49:51 +0000

    Update Spring security jars to 3.0.2 and modified some codes

--- Workin/workin/org/workin/spring/security/SpringSecurityUtils.java
+++ Workin/workin/org/workin/spring/security/SpringSecurityUtils.java
@@ -1,8 +1,18 @@
 package org.workin.spring.security;
 
-import org.springframework.security.Authentication;
-import org.springframework.security.context.SecurityContextHolder;
-import org.springframework.security.userdetails.User;
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
 
 /**
  * 
@@ -11,38 +21,93 @@ import org.springframework.security.userdetails.User;
  */
 public class SpringSecurityUtils {
 	
-	private SpringSecurityUtils() {
-		
-	}
-	
-	/**
-	 * 
-	 * get spring's user.
-	 * @return
-	 * 
-	 */
-	public static String getCurrentUserName() {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-		if (auth == null)
-			return "";
-
-		return auth.getName();
-	}
-	
-	/**
-	 * 
-	 * get spring's user or user's subclass instance.
-	 * 
-	 * @param <T>
-	 * @return
-	 */
 	@SuppressWarnings("unchecked")
 	public static <T extends User> T getCurrentUser() {
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		if (principal == null)
-			return null;
-		return (T) principal;
+		Authentication authentication = getAuthentication();
+		if (authentication != null) {
+			Object principal = authentication.getPrincipal();
+			if (principal instanceof User) {
+				return (T) principal;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * Get the current user's login name, if the current user is not logged returns an empty string.
+	 * 
+	 * @return
+	 */
+	public static String getCurrentUserName() {
+		Authentication authentication = getAuthentication();
+		if (authentication != null && authentication.getPrincipal() != null) {
+			return authentication.getName();
+		}
+		return "";
 	}
 	
+	/**
+	 * 
+	 * Get the current user logged IP, if the current user is not logged returns an empty string.
+	 * 
+	 */
+	public static String getCurrentUserIp() {
+		Authentication authentication = getAuthentication();
+		if (authentication != null) {
+			Object details = authentication.getDetails();
+			if (details instanceof WebAuthenticationDetails) {
+				WebAuthenticationDetails webDetails = (WebAuthenticationDetails) details;
+				return webDetails.getRemoteAddress();
+			}
+		}
+
+		return "";
+	}
+
+	/**
+	 * 
+	 * Determine whether the user has a role, if any of the parameters the user has a role in the return to true.
+	 * 
+	 */
+	public static boolean hasAnyRole(String[] roles) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Collection<GrantedAuthority> granteds = authentication.getAuthorities();
+		for (String role : roles) {
+			for (GrantedAuthority authority : granteds) {
+				if (role.equals(authority.getAuthority())) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 
+	 * Save userDetails to Context.
+	 * 
+	 * @param userDetails
+	 * @param request
+	 * 
+	 */
+	public static void saveUserDetailsToContext(UserDetails userDetails, HttpServletRequest request) {
+		PreAuthenticatedAuthenticationToken authentication = new PreAuthenticatedAuthenticationToken(userDetails,
+				userDetails.getPassword(), userDetails.getAuthorities());
+		authentication.setDetails(new WebAuthenticationDetails(request));
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+
+	/**
+	 * 
+	 * @return
+	 * 
+	 */
+	private static Authentication getAuthentication() {
+		SecurityContext context = SecurityContextHolder.getContext();
+		if (context != null) {
+			return context.getAuthentication();
+		}
+		return null;
+	}
 }
