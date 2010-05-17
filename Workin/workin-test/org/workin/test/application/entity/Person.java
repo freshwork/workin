04769ad5f43ad9c53912992e29b401270532c534
goingmm@gmail.com <goingmm@gmail.com@243312a4-f120-11de-a719-13ef0a9ea6ad>
2010-05-17 06:47:16 +0000

    Modified EntityComparator to fixed min bug

--- Workin/workin-test/org/workin/test/application/entity/Person.java
+++ Workin/workin-test/org/workin/test/application/entity/Person.java
@@ -1,7 +1,10 @@
 package org.workin.test.application.entity;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -19,6 +22,7 @@ import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.workin.core.entity.IdEntity;
 import org.workin.util.CollectionUtils;
+import org.workin.util.DateUtils;
 
 /**
  * 
@@ -26,110 +30,123 @@ import org.workin.util.CollectionUtils;
  *
  */
 @Entity
-@Table(name="person")
-@NamedQueries({
-		@NamedQuery(name="findAllPerson", query="SELECT p FROM Person p"),
-		@NamedQuery(name="findPersonByName", query="SELECT p FROM Person p WHERE p.name = :name"),
-		@NamedQuery(name="findPersonByNameExt", query="SELECT p FROM Person p WHERE p.name = ?"),
-		@NamedQuery(name="update_Person", query="UPDATE Person SET name = ? WHERE name = ?"),
-		@NamedQuery(name="del_Person", query="DELETE FROM Person WHERE name = ?")
-		})
-@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)		
+@Table(name = "person")
+@NamedQueries( { @NamedQuery(name = "findAllPerson", query = "SELECT p FROM Person p"),
+		@NamedQuery(name = "findPersonByName", query = "SELECT p FROM Person p WHERE p.name = :name"),
+		@NamedQuery(name = "findPersonByNameExt", query = "SELECT p FROM Person p WHERE p.name = ?"),
+		@NamedQuery(name = "update_Person", query = "UPDATE Person SET name = ? WHERE name = ?"),
+		@NamedQuery(name = "del_Person", query = "DELETE FROM Person WHERE name = ?") })
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Person extends IdEntity {
-	
+
 	private static final long serialVersionUID = 6060042527393306255L;
-	
+
 	private String name;
-	
+
 	private String sex;
-	
+
 	private Date birthday;
-	
+
 	private Set<Address> address = new LinkedHashSet<Address>();
-	
-	@OneToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY)  
+
+	private Double sortTest;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 	public Set<Address> getAddress() {
 		return address;
 	}
+
 	public void setAddress(Set<Address> address) {
 		this.address = address;
 	}
-	
-	@Column(length=30)
+
+	@Column(length = 30)
 	public Date getBirthday() {
 		return birthday;
 	}
+
 	public void setBirthday(Date birthday) {
 		this.birthday = birthday;
 	}
-	
-	@Column(length=20)
+
+	@Column(length = 20)
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	@Column(length=10)
+
+	@Column(length = 10)
 	public String getSex() {
 		return sex;
 	}
+
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
-	
+
 	@Transient
 	public String getAddressPhone() {
 		return CollectionUtils.fetchPropertyToString(address, "phone", ", ");
 	}
-	
+
+	@Transient
+	public Double getSortTest() {
+		return sortTest;
+	}
+
+	public void setSortTest(Double sortTest) {
+		this.sortTest = sortTest;
+	}
+
 	@Override
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this);
 	}
-	
+
 	@Transient
-	public void setDynCompareValue(){
-		this.setDynCompareField(this.birthday);
+	public void setDynCompareValue() {
+		this.setDynCompareField(this.sortTest);
 	}
-	
-//	@SuppressWarnings("deprecation")
-//	public static void main(String[] args) {
-//	
-//		Person wanggang = new Person();
-//		wanggang.setId(1L);
-//		wanggang.setName("c");
-//		wanggang.setBirthday(DateUtils.getNow());
-//		
-//		Person angellin = new Person();
-//		angellin.setId(2L);
-//		angellin.setName("b");
-//		angellin.setBirthday(new Date("2008/12/30"));
-//		
-//		Person goingmm = new Person();
-//		goingmm.setId(3L);
-//		goingmm.setName("a");
-//		goingmm.setBirthday(new Date("2009/12/30"));
-//		
-//		List<Person> list = new ArrayList<Person>();
-//		list.add(wanggang);
-//		list.add(angellin);
-//		list.add(goingmm);
-//		
-//		Collections.sort(list, ascComparator);
-//		
-//		for(Person person : list) {
-//			System.out.println(" id: " + person.getId() + " name: " + person.getName() + " birthday: " + person.getBirthday());
-//		}
-//		
-//		System.out.println(" ------------------------ ");
-//		
-//		Collections.sort(list, descComparator);
-//		
-//		for(Person person : list) {
-//			System.out.println(" id: " + person.getId() + " name: " + person.getName() + " birthday: " + person.getBirthday());
-//		}
-//	}
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		Person wanggang = new Person();
+		wanggang.setId(101L);
+		wanggang.setName("c");
+		wanggang.setBirthday(DateUtils.getNow());
+		wanggang.setSortTest(10.1);
+
+		Person angellin = new Person();
+		angellin.setId(78L);
+		angellin.setName("b");
+		angellin.setBirthday(new Date("2008/12/30"));
+		angellin.setSortTest(10.7);
+
+		Person goingmm = new Person();
+		goingmm.setId(11L);
+		goingmm.setName("a");
+		goingmm.setBirthday(new Date("2009/12/30"));
+		goingmm.setSortTest(10.3);
+		
+		List<Person> list = new ArrayList<Person>();
+		list.add(wanggang);
+		list.add(angellin);
+		list.add(goingmm);
+
+		Collections.sort(list, ascComparator);
+		for (Person person : list) {
+			System.out.println(" id: " + person.getId() + " name: " + person.getName() + " birthday: "
+					+ person.getBirthday() + " sortTest: " + person.getSortTest());
+		}
+		System.out.println(" ------------------------ ");
+		Collections.sort(list, descComparator);
+		for (Person person : list) {
+			System.out.println(" id: " + person.getId() + " name: " + person.getName() + " birthday: "
+					+ person.getBirthday());
+		}
+	}
 }
