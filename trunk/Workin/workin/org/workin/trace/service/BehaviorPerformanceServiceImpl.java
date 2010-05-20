67fc6c926a6d4707a7592b2ed259d2cd36fe0ad1
goingmm <goingmm@243312a4-f120-11de-a719-13ef0a9ea6ad>
2010-05-20 06:40:23 +0000

    Added @Service

--- trunk/Workin/workin/org/workin/trace/service/BehaviorPerformanceServiceImpl.java
+++ trunk/Workin/workin/org/workin/trace/service/BehaviorPerformanceServiceImpl.java
@@ -2,6 +2,7 @@ package org.workin.trace.service;
 
 import java.io.Serializable;
 
+import org.springframework.stereotype.Service;
 import org.workin.core.persistence.support.AbstractBeanService;
 import org.workin.trace.domain.BehaviorPerformance;
 
@@ -10,6 +11,7 @@ import org.workin.trace.domain.BehaviorPerformance;
  * @author <a href="mailto:goingmm@gmail.com">G.Lee</a>
  *
  */
+@Service
 public class BehaviorPerformanceServiceImpl extends AbstractBeanService<BehaviorPerformance, Serializable> implements
 		BehaviorPerformanceService {
 
