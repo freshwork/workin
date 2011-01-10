6eb62a6c444137746c882adcd133fac94b34ca55
yao.angellin <yao.angellin@243312a4-f120-11de-a719-13ef0a9ea6ad>
2011-01-10 09:12:20 +0000

    add execute procedure method in JPA implements

--- Workin/workin-test/org/workin/test/application/service/PersonServiceImpl.java
+++ Workin/workin-test/org/workin/test/application/service/PersonServiceImpl.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 import org.workin.core.persistence.support.AbstractBeanService;
 import org.workin.core.persistence.support.PaginationSupport;
+import org.workin.core.persistence.support.ProcedureParameter;
 import org.workin.test.application.entity.Person;
-
 
 /**
  * 
@@ -15,22 +15,22 @@ import org.workin.test.application.entity.Person;
  */
 @SuppressWarnings("unchecked")
 public class PersonServiceImpl extends AbstractBeanService implements PersonService {
-	
+
 	@Override
 	public List findPersonsBySqlMap(String sqlMapId, Person person) {
 		return this.getPersistenceService().findListBySqlMap(sqlMapId, person);
 	}
-	
+
 	@Override
 	public List findPersonsByNamedQuery(String namedOfQuery, Map nameAndValue) {
 		return this.getPersistenceService().findByNamedOfQuery(namedOfQuery, nameAndValue);
 	}
-	
+
 	@Override
 	public List findPersonsByProperty(String propertyName, Object value) {
 		return this.getPersistenceService().findByProperty(Person.class, propertyName, value);
 	}
-	
+
 	@Override
 	public List findPersonsByPropertys(Map nameAndValue) {
 		return this.getPersistenceService().findByPropertys(Person.class, nameAndValue);
@@ -45,24 +45,30 @@ public class PersonServiceImpl extends AbstractBeanService implements PersonServ
 	public int persistByNativeQuery(String queryString) {
 		return this.getPersistenceService().persistByNativeQuery(queryString);
 	}
-	
+
 	@Override
 	public int persistByNativeQuery(final String queryString, final Object... values) {
 		return this.getPersistenceService().persistByNativeQuery(queryString, values);
 	}
-	
+
 	@Override
 	public PaginationSupport findPersonsBySqlMap(String sqlMapId, Person person, int offset, int maxRows) {
 		return this.getPersistenceService().findPaginatedBySqlMap(sqlMapId, person, offset, maxRows);
 	}
-	
+
 	@Override
 	public List find(final String queryString, final Object... values) {
 		return this.getPersistenceService().find(queryString, values);
 	}
-	
+
 	@Override
-	public PaginationSupport findPaginationSupport(final int start, final int maxRows, final String queryString, final Object... values) {
+	public PaginationSupport findPaginationSupport(final int start, final int maxRows, final String queryString,
+			final Object... values) {
 		return this.getPersistenceService().findPaginationSupport(start, maxRows, queryString, values);
+	}
+
+	@Override
+	public Map<String, Object> executeProcedure(String procName, List<ProcedureParameter> procParams) {
+		return this.getPersistenceService().executeProcedure(procName, procParams);
 	}
 }
